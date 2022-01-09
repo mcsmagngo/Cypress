c11a4e689d675d18ae02976993daf41f758ad8c2
Mauricio <mcsmagnago@gmail.com>
2022-01-08 22:12:17 -0300

    projeto cypress

---
+++ cypress/integration/gui/login.spec.js
@@ -0,0 +1,9 @@
+/// <reference types = "Cypress" />
+
+describe("Login", () => {
+    it('sucessfully', () => {
+        cy.login()
+
+        cy.get('.qa-user-avatar').should('exist')
+    })
+})
---
+++ cypress/integration/gui/logout.spec.js
@@ -0,0 +1,12 @@
+/// <reference types = "Cypress" />
+
+describe("logout", () => {
+    beforeEach(() => cy.login())
+
+    it('sucessfully', () => {
+        cy.logout()
+
+        cy.url().should('be.equal', `${Cypress.config('baseUrl')}users/sign_in`)
+
+    })
+})
